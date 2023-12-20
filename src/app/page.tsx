96a861c520f63ae27126a540a4a253960083fb97
Sim <0doogadooga@gmail.com>
2023-12-20 18:59:35 +0900

    #8 Button global component 완료

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import S from "./page.module.css";
 import { CategoryTitle } from "@/components/main/categoryTitle/CategoryTitle";
 import { mockData } from "./mock";
 import Item from "@/components/main/Item/Item";
+import Button from "@/components/common/button/Button";
 export default function Home() {
   /**Item render하는 함수 */
   const renderItem = () => {
@@ -16,6 +17,15 @@ export default function Home() {
 
   return (
     <div className={S.mainWrap}>
+      <Button
+        desc="버튼이에요"
+        size="big"
+        bg="#00305B"
+        color="#fff"
+        bold={700}
+      />
+      <Button desc="버튼이에요" size="medium" bg="#9BE400" bold={300} />
+      <Button desc="버튼이에요" size="small" color="#FA6C67" />
       <header className={S.headerWrap}>헤더입니다.</header>
       <NaviBar />
       <main className={S.itemContainerWrap}>
--- src/components/common/button/Button.tsx
+++ src/components/common/button/Button.tsx
@@ -1,3 +1,19 @@
-export function Button() {
-  return <button>button</button>;
+"use client";
+import React from "react";
+import { PropsWithChildren } from "react";
+import * as S from "./Button.style";
+
+interface ButtonProps {
+  desc: string;
+  size: "big" | "medium" | "small";
+  bg?: string;
+  color?: string;
+  bold?: number;
+}
+export default function Button({ desc, size, ...rest }: ButtonProps) {
+  return (
+    <S.Button size={size} {...rest}>
+      {desc}
+    </S.Button>
+  );
 }
