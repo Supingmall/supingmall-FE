848c08805a049daf7dfe51c82e227122657c1502
하방방 <81242485+jumosd@users.noreply.github.com>
2023-12-22 20:35:46 +0900

    input태그에 value값 넣으면 생기는 오류수정
    input 태그에는 value값만 넣엇다가 onChange()속성을 추가했습니다.

--- src/components/common/counter/Counter.tsx
+++ src/components/common/counter/Counter.tsx
@@ -10,21 +10,29 @@ const Counter = ({ width, height }: CounterProps) => {
     const [value, setValue] = useState(0)
 
     const plusHandler = () => {
-        setValue(value + 1)
+        setValue(value + 1);
     }
+
     const minusHandler = () => {
         if (!value) {
-            return
+            return;
         }
-        setValue(value - 1)
+        setValue(value - 1);
     }
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setValue(Number(event.target.value));
+    }
+
     return (
         <div className={style["detail-head__counter"]} style={{ width, height }}>
-            <div className={style["detail-head__counter_button"]} onClick={plusHandler} >
-                <img className={style["detail-head__counter_button_image"]} src="/detailpage/plusButton.svg" alt="" /></div>
-            <input className={style["detail-head__counter_value"]} value={value} />
             <div className={style["detail-head__counter_button"]} onClick={minusHandler}>
-                <img className={style["detail-head__counter_button_image"]} src="/detailpage/minusButton.svg" alt="" /></div>
+                <img className={style["detail-head__counter_button_image"]} src="/detailpage/minusButton.svg" alt="" />
+            </div>
+            <input className={style["detail-head__counter_value"]} value={value} onChange={handleInputChange} />
+            <div className={style["detail-head__counter_button"]} onClick={plusHandler}>
+                <img className={style["detail-head__counter_button_image"]} src="/detailpage/plusButton.svg" alt="" />
+            </div>
         </div>
     );
 };
