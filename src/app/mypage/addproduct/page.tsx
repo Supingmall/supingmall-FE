e9032d503b908baa8058c8eb33b1bb79bbb22112
YukDDong <ekfhd5537@naver.com>
2023-12-24 15:05:59 +0900

    feat : 판매현황 페이지 UI 작업

---
+++ src/app/mypage/addproduct/page.tsx
@@ -0,0 +1,12 @@
+import styles from "./page.module.css";
+
+export default function AddProduct() {
+  return (
+    <section className={styles.container}>
+      <div className={styles.header}>
+        <h2>상품 등록</h2>
+      </div>
+      <form></form>
+    </section>
+  );
+}
---
+++ src/app/mypage/salesstatus/page.tsx
@@ -0,0 +1,15 @@
+import styles from "./page.module.css";
+
+export default function SalesStatus() {
+  return (
+    <section className={styles.container}>
+      <div className={styles.header}>
+        <h2>판매 현황</h2>
+        <input />
+        {/* 검색 창 들어갈 자리 */}
+      </div>
+      <div>테이블 자리</div>
+      {/* 테이블 컴포넌트 들어갈 자리 */}
+    </section>
+  );
+}
