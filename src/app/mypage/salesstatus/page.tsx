3079d101f5c163bdfb7757cbe0ffb1f2844da077
YukDDong <ekfhd5537@naver.com>
2023-12-24 17:18:02 +0900

    feat : 판매현황 페이지

--- src/app/mypage/salesstatus/page.tsx
+++ src/app/mypage/salesstatus/page.tsx
@@ -1,3 +1,4 @@
+import SalesTable from "@/components/mypage/SalesTable/SalesTable";
 import styles from "./page.module.css";
 
 export default function SalesStatus() {
@@ -8,8 +9,7 @@ export default function SalesStatus() {
         <input />
         {/* 검색 창 들어갈 자리 */}
       </div>
-      <div>테이블 자리</div>
-      {/* 테이블 컴포넌트 들어갈 자리 */}
+      <SalesTable />
     </section>
   );
 }
