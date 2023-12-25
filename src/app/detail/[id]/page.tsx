e99ab1ecca978a2108357b1403c262e5cb01094a
하방방 <81242485+jumosd@users.noreply.github.com>
2023-12-26 07:48:20 +0900

    jsonserver로 상세페이지 테스트하기

--- src/app/detail/[id]/page.tsx
+++ src/app/detail/[id]/page.tsx
@@ -1,13 +1,16 @@
+import { fetchData } from "@/app/apis/detailageApi";
 import DetailHead from "@/components/detail/DetailHead";
+import { isDataView } from "util/types";
 
-const Detail = (props: any) => {
-    console.log(props)
-    console.log("test")
+const Detail = async (props: any) => {
 
+    // const url = `/v1/api/product-detail/${props.params.id}`
+    const url = `http://localhost:9999/item`
+    const data = await fetchData(url)
     return (
         <div>
             <div>
-                <DetailHead />
+                {data ? <DetailHead testData={data} /> : "데이터안옴"}
             </div>
         </div>
     );
--- src/components/detail/DetailHead.tsx
+++ src/components/detail/DetailHead.tsx
@@ -5,58 +5,59 @@ import Counter from "../common/counter/Counter"
 import StarPoint from "../common/starpoint/StarPoint";
 import "./DetailHead.css"
 import Selector from "./Selector";
-import testData from './test.json';
 import DetailReview from "./DetailReview";
 
-const productDetailList = testData.productDetailList
-const productReview = testData.productReview
+const DetailHead = ({ testData }: any) => {
 
-const DetailHead = () => {
-
-
-
-
-    return (
-        <div className='detailpage'>
-            <div className='detail-head__container'>
-                <div className='detail-head__imagebox'>
-                    <img className='detail-head__image' src="/detailpage/testimage.jpg" alt="product_img" />
-                </div>
-                <div className='detail-head__description_area'>
-                    <div >
-                        <h2>제품이름</h2>
+    if (testData) {
+        return (
+            <div className='detailpage'>
+                <div className='detail-head__container'>
+                    <div className='detail-head__imagebox'>
+                        <img className='detail-head__image' src="/detailpage/testimage.jpg" alt="product_img" />
                     </div>
-                    <div>
-                        평점 : {testData.scoreAvg} <StarPoint starPoint={testData.scoreAvg} setStarPoint={() => { }} />
-                    </div>
-                    <div>
-                        가격 : {testData.productPrice}
-                    </div>
-                    <div>
-                        색상
-                        <Selector item={productDetailList.map(item => item['color'])} />
-                    </div>
-                    {/* 사이즈 컴포넌트로 대체 */}
-                    <div>
-                        사이즈
-                        <Selector item={productDetailList.map(item => item['product_size'])} />
-                    </div>
-                    {/* 맥스는 재고수량까지 */}
-                    <Counter width={"300px"} height={"40px"} />
-                    {/* 버튼들 */}
-                    <div className='detail-head__buttonbox'>
-                        <button>구매하기</button>
-                        <button >장바구니</button>
+                    <div className='detail-head__description_area'>
+                        <div >
+                            <h2>제품이름</h2>
+                        </div>
+                        <div>
+                            평점 : {testData.scoreAvg} <StarPoint starPoint={testData.scoreAvg} setStarPoint={() => { }} />
+                        </div>
+                        <div>
+                            가격 : {testData.productPrice}
+                        </div>
+                        <div>
+                            색상
+                            <Selector item={testData.productDetailList.map(item => item['color'])} />
+                        </div>
+                        {/* 사이즈 컴포넌트로 대체 */}
+                        <div>
+                            사이즈
+                            <Selector item={testData.productDetailList.map(item => item['product_size'])} />
+                        </div>
+                        {/* 맥스는 재고수량까지 */}
+                        <Counter width={"300px"} height={"40px"} />
+                        {/* 버튼들 */}
+                        <div className='detail-head__buttonbox'>
+                            <button>구매하기</button>
+                            <button >장바구니</button>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <DetailReview reviewlist={productReview} />
-            {/* 제품설명 상세이미지 */}
-            <div className='detail-body__deatil_image_container'>
-                <img className='detail-body__deatil_image' src="/detailpage/detailimage.jpeg" alt="" />
-            </div>
-        </div >
-    );
+                <DetailReview reviewlist={testData.productReview} />
+                {/* 제품설명 상세이미지 */}
+                <div className='detail-body__deatil_image_container'>
+                    <img className='detail-body__deatil_image' src="/detailpage/detailimage.jpeg" alt="" />
+                </div>
+            </div >
+        );
+    } else {
+        return (
+            <>
+                데이터안옴
+            </>
+        )
+    }
 };
 
 export default DetailHead;
