332ab6046c7290b19aa9072aac6a7ae02d05ccd5
하방방 <81242485+jumosd@users.noreply.github.com>
2023-12-26 22:37:21 +0900

    상세페이지css 모듈 적용

--- src/components/detail/DetailHead.tsx
+++ src/components/detail/DetailHead.tsx
@@ -3,9 +3,9 @@
 import { useRef, useState } from "react";
 import Counter from "../common/counter/Counter"
 import StarPoint from "../common/starpoint/StarPoint";
-import "./DetailHead.css"
 import Selector from "./Selector";
 import DetailReview from "./DetailReview";
+import style from "./DetailHead.module.css"
 
 const DetailHead = ({ testData }: any) => {
     const option1 = useRef(null)
@@ -21,17 +21,20 @@ const DetailHead = ({ testData }: any) => {
     }
     if (testData) {
         return (
-            <div className='detailpage'>
-                <div className='detail-head__container'>
-                    <div className='detail-head__imagebox'>
-                        <img className='detail-head__image' src="/detailpage/testimage.jpg" alt="product_img" />
+            <div className={style["detailpage"]}>
+                <div className={style['detail-head__container']}>
+                    <div className={style['detail-head__imagebox']}>
+                        <img className={style['detail-head__image']} src="/detailpage/testimage.jpg" alt="product_img" />
                     </div>
-                    <div className='detail-head__description_area'>
+                    <div className={style['detail-head__description_area']}>
                         <div >
                             <h2>제품이름</h2>
                         </div>
                         <div>
-                            평점 : {testData.scoreAvg} <StarPoint starPoint={testData.scoreAvg} setStarPoint={() => { }} />
+                            <div className={style["starpoint-container"]}>
+
+                                평점 : {testData.scoreAvg} <StarPoint starPoint={testData.scoreAvg} setStarPoint={() => { }} />
+                            </div>
                         </div>
                         <div>
                             가격 : {testData.productPrice}
@@ -48,7 +51,7 @@ const DetailHead = ({ testData }: any) => {
                         {/* 맥스는 재고수량까지 */}
                         <Counter ref={option3} width={"300px"} height={"40px"} />
                         {/* 버튼들 */}
-                        <div className='detail-head__buttonbox'>
+                        <div className={style['detail-head__buttonbox']}>
                             <button>구매하기</button>
                             <button onClick={shoppingBasket}>장바구니</button>
                         </div>
@@ -56,8 +59,8 @@ const DetailHead = ({ testData }: any) => {
                 </div>
                 <DetailReview reviewlist={testData.productReview} />
                 {/* 제품설명 상세이미지 */}
-                <div className='detail-body__deatil_image_container'>
-                    <img className='detail-body__deatil_image' src="/detailpage/detailimage.jpeg" alt="" />
+                <div className={style['detail-body__deatil_image_container']}>
+                    <img className={style['detail-body__deatil_image']} src="/detailpage/detailimage.jpeg" alt="" />
                 </div>
             </div >
         );
