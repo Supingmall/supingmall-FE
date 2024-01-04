0597a1eeff6a51f24ff6d11e86ec11305abc2c8b
swp91 <seongwoo4477@gmail.com>
2024-01-04 14:07:21 +0900

    feat:내정보, 구매내역 api 연동

--- src/components/detail/DetailHead.tsx
+++ src/components/detail/DetailHead.tsx
@@ -1,85 +1,92 @@
-"use client"
+"use client";
 
 import { useRef, useState } from "react";
-import Counter from "../common/counter/Counter"
+import Counter from "../common/counter/Counter";
 import StarPoint from "../common/starpoint/StarPoint";
 import Selector from "./Selector";
 import DetailReview from "./DetailReview";
-import style from "./DetailHead.module.css"
+import style from "./DetailHead.module.css";
 import SelectRadio from "../common/selectradio/SelectRadio";
 import { shoppingBasketAPI } from "@/app/apis/detailageApi";
 import { url } from "inspector";
 import axios from "axios";
 
 const DetailHead = ({ testData }: any) => {
-    console.log(testData)
-    const [selectOptionId, setSelectOptionId] = useState<number | null>(null)
-    const amount = useRef<HTMLInputElement | null>(null)
-    const shoppingBasket = async () => {
-        try {
-            const cleanToken = (token: string | null) => {
-                return token ? token.replace(/["']/g, '') : null;
-            }
-            const token = cleanToken(window.localStorage.getItem("Token"));
-            const body = {
-                "product_option_id": selectOptionId,
-                "add_amount": Number(amount.current?.value),
-            }
-            const url = `http://ec2-52-79-235-118.ap-northeast-2.compute.amazonaws.com:8080/v1/api/cart`
-            const response = await shoppingBasketAPI(url, body, token)
-            console.log(response);
-            console.log(response.data);
-        } catch (error) {
-            console.error('Error', error);
-        }
+  console.log(testData);
+  const [selectOptionId, setSelectOptionId] = useState<number | null>(null);
+  const amount = useRef<HTMLInputElement | null>(null);
+  const shoppingBasket = async () => {
+    try {
+      const cleanToken = (token: string | null) => {
+        return token ? token.replace(/["']/g, "") : null;
+      };
+      const token = cleanToken(window.localStorage.getItem("Token"));
+      const body = {
+        product_option_id: selectOptionId,
+        add_amount: Number(amount.current?.value),
+      };
+      const url = `http://ec2-52-79-235-118.ap-northeast-2.compute.amazonaws.com:8080/v1/api/cart`;
+      const response = await shoppingBasketAPI(url, body, token);
+      console.log(response);
+      console.log(response.data);
+    } catch (error) {
+      console.error("Error", error);
     }
+  };
 
-    if (testData) {
-        return (
-            <div className={style["detailpage"]}>
-                <div className={style['detail-head__container']}>
-                    <div className={style['detail-head__imagebox']}>
-                        <img className={style['detail-head__image']} src={testData.productPhoto
-                        [0].photo_url} alt="product_img" />
-                    </div>
-                    <div className={style['detail-head__description_area']}>
-                        <div >
-                            <h2>{testData.productName}</h2>
-                        </div>
-                        <div>
-                            <div className={style["starpoint-container"]}>
-                                평점 : {testData.scoreAvg} <StarPoint starPoint={testData.scoreAvg} setStarPoint={() => { }} />
-                            </div>
-                        </div>
-                        <div>
-                            가격 : {testData.productPrice}
-                        </div>
-                        {/* 옵션선택 라디오컴포넌트  */}
-                        <SelectRadio optionList={testData.productDetailList} option={setSelectOptionId} />
+  if (testData) {
+    return (
+      <div className={style["detailpage"]}>
+        <div className={style["detail-head__container"]}>
+          <div className={style["detail-head__imagebox"]}>
+            <img
+              className={style["detail-head__image"]}
+              src={testData.productPhoto[0].photo_url}
+              alt="product_img"
+            />
+          </div>
+          <div className={style["detail-head__description_area"]}>
+            <div>
+              <h2>{testData.productName}</h2>
+            </div>
+            <div>
+              <div className={style["starpoint-container"]}>
+                평점 : {testData.scoreAvg}{" "}
+                <StarPoint
+                  starPoint={testData.scoreAvg}
+                  setStarPoint={() => {}}
+                />
+              </div>
+            </div>
+            <div>가격 : {testData.productPrice}</div>
+            {/* 옵션선택 라디오컴포넌트  */}
+            <SelectRadio
+              optionList={testData.productDetailList}
+              option={setSelectOptionId}
+            />
 
-                        {/* 맥스는 재고수량까지 */}
-                        <Counter ref={amount} width={"300px"} height={"40px"} />
-                        {/* 버튼들 */}
-                        <div className={style['detail-head__buttonbox']}>
-                            <button onClick={shoppingBasket}>장바구니</button>
-                        </div>
-                    </div>
-                </div>
-                <DetailReview reviewlist={testData.productReview} />
-                {/* 제품설명 상세이미지 */}
-                <div className={style['detail-body__deatil_image_container']}>
-                    <img className={style['detail-body__deatil_image']} src={testData.productPhoto
-                    [1].photo_url} alt="" />
-                </div>
-            </div >
-        );
-    } else {
-        return (
-            <>
-                데이터안옴
-            </>
-        )
-    }
+            {/* 맥스는 재고수량까지 */}
+            <Counter ref={amount} width={"300px"} height={"40px"} />
+            {/* 버튼들 */}
+            <div className={style["detail-head__buttonbox"]}>
+              <button onClick={shoppingBasket}>장바구니</button>
+            </div>
+          </div>
+        </div>
+        <DetailReview reviewlist={testData.productReview} />
+        {/* 제품설명 상세이미지 */}
+        <div className={style["detail-body__deatil_image_container"]}>
+          <img
+            className={style["detail-body__deatil_image"]}
+            src={testData.productPhoto[1].photo_url}
+            alt=""
+          />
+        </div>
+      </div>
+    );
+  } else {
+    return <>데이터안옴</>;
+  }
 };
 
 export default DetailHead;
--- src/store/MypageAtom.ts
+++ src/store/MypageAtom.ts
@@ -1,31 +1,5 @@
 import { atom } from "recoil";
 
-export interface ProductOption {
-  option_id: number;
-  color: string;
-  size: string;
-  price: number;
-  quantity: number;
-}
-
-export interface ProductResponse {
-  product_id: number;
-  product_name: string;
-  product_img: string[];
-  product_option_response: ProductOption[];
-  product_total_price: number;
-}
-
-export interface Order {
-  order_id: number;
-  order_status: string;
-  ship: string;
-  order_request: string;
-  order_at: string;
-  product_response_list: ProductResponse[];
-  order_total_price: number;
-}
-
 interface UserData {
   name: string;
   gender: string;
@@ -59,99 +33,34 @@ export const mydataAtom = atom<UserData>({
     user_roles: [],
   },
 });
+export interface ProductOption {
+  option_id: number;
+  color?: string;
+  size?: string;
+  price?: number;
+  quantity?: number;
+}
+
+export interface ProductResponse {
+  product_id: number;
+  product_name?: string;
+  product_img?: string[];
+  product_option_response?: ProductOption[];
+  product_total_price?: number;
+}
+
+export interface Order {
+  order_id: number;
+  order_status?: string;
+  ship?: string;
+  order_request?: string;
+  order_at?: string;
+  product_response_list?: ProductResponse[];
+  order_total_price?: number;
+}
 
 //내 구매내역
 export const orderitemAtom = atom<Order[]>({
   key: "orderitemAtom",
-  default: [
-    {
-      //하나의 주문 안에 같은상품의 여러옵션을 구매했을경우 아래처럼 하나의 상품에 옵션을 여러개로 담아 반환 합니다.
-      order_id: 1,
-      order_status: "배송중",
-      ship: "서울",
-      order_request: "문앞",
-      order_at: "2023년 12월 17일 - 19시 07분",
-      product_response_list: [
-        {
-          product_id: 6,
-          product_name: "유행지난 후드티",
-          product_img: ["대표상품사진1"],
-          product_option_response: [
-            {
-              option_id: 8,
-              color: "흰색",
-              size: "M",
-              price: 500,
-              quantity: 1,
-            },
-            {
-              option_id: 9,
-              color: "파랑",
-              size: "XL",
-              price: 500,
-              quantity: 1,
-            },
-          ],
-          product_total_price: 1000, //상품별 갯수, 옵션을 합한 총결제금액
-        },
-        {
-          product_id: 7,
-          product_name: "후줄근한 바지",
-          product_img: ["대표상품사진1"],
-          product_option_response: [
-            {
-              option_id: 10,
-              color: "투명",
-              size: "L",
-              price: 300,
-              quantity: 2,
-            },
-          ],
-          product_total_price: 600,
-        },
-      ],
-      order_total_price: 1600, // 주문별 총 금액
-    },
-    {
-      order_id: 2,
-      order_status: "배송완료",
-      ship: "서울",
-      order_request: "",
-      order_at: "2023년 12월 13일 - 19시 07분",
-      product_response_list: [
-        {
-          product_id: 8,
-          product_name:
-            "나이키 에어 조던 11 DMP 그래티튜드 CT8012-170 378038-170 데일리슈즈 한정판",
-          product_img: ["대표상품사진1"],
-          product_option_response: [
-            {
-              option_id: 14,
-              color: "흰색",
-              size: "260",
-              price: 219000,
-              quantity: 1,
-            },
-          ],
-          product_total_price: 219000,
-        },
-        {
-          product_id: 9,
-          product_name: "목 늘어난 티셔츠",
-          product_img: ["대표상품사진1"],
-          product_option_response: [
-            {
-              option_id: 17,
-              color: "노랑",
-              size: "L",
-              price: 3000,
-              quantity: 1,
-            },
-          ],
-          product_total_price: 3000,
-        },
-      ],
-      order_total_price: 222000,
-    },
-  ],
+  default: [],
 });
